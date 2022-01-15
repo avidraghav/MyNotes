f3addcf6fee6dc141bb3e1cb510c253305ac6e82
Raghav Aggarwal <raghavaggarwal776@gmail.com>
2022-01-15 13:05:33 +0530

    Added Resource.kt class and progressbar in AddTaskFragment.kt

--- app/src/main/java/com/raghav/mvvmtodo/AddTaskFragment.kt
+++ app/src/main/java/com/raghav/mvvmtodo/AddTaskFragment.kt
@@ -2,6 +2,9 @@ package com.raghav.mvvmtodo
 
 import android.os.Bundle
 import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
@@ -21,14 +24,30 @@ class AddTaskFragment : Fragment(R.layout.fragment_add_task) {
         binding = FragmentAddTaskBinding.bind(view)
 
         binding.btnSaveTask.setOnClickListener {
-            viewLifecycleOwner.lifecycleScope.launchWhenStarted {
-                val title = binding.etTaskTitle.text.toString()
-                val description = binding.description.text.toString()
-                val aTask = TaskEntity(title, description)
-                viewModel.saveTask(aTask)
-                findNavController().navigate(R.id.action_addTaskFragment_to_allTasksFragment)
-            }
+            val title = binding.etTaskTitle.text.toString()
+            val description = binding.description.text.toString()
+            val aTask = TaskEntity(title, description)
+            saveTask(aTask)
 
         }
+    }
+
+    private fun saveTask(aTask: TaskEntity) {
+        viewLifecycleOwner.lifecycleScope.launchWhenStarted {
+            showProgressBar()
+            viewModel.saveTask(aTask)
+            hideProgressBar()
+            Toast.makeText(activity, "Task Saved", Toast.LENGTH_SHORT).show()
+            findNavController().navigate(R.id.action_addTaskFragment_to_allTasksFragment)
+        }
+
+    }
+
+    private fun hideProgressBar() {
+        binding.progressbar.visibility = VISIBLE
+    }
+
+    private fun showProgressBar() {
+        binding.progressbar.visibility = GONE
     }
 }
---
+++ app/src/main/java/com/raghav/mvvmtodo/utils/Resource.kt
@@ -0,0 +1,8 @@
+package com.raghav.mvvmtodo.utils
+
+sealed class Resource<T>(val data: T? = null, val message: String? = null) {
+
+    class Success<T>(data: T?) : Resource<T>(data)
+    class Loading<T> : Resource<T>()
+    class Error<T>(message: String, data: T? = null) : Resource<T>(data, message)
+}
