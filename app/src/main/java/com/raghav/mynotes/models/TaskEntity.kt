0372cbedab8019290c16e592c68bb3fc4a221ede
Krish Parekh <Krishp4123@gmail.com>
2022-10-06 23:04:49 +0530

    NoteApp : Modified TaskEntity.kt from Serializable to Parcelable

--- app/src/main/java/com/raghav/mynotes/models/TaskEntity.kt
+++ app/src/main/java/com/raghav/mynotes/models/TaskEntity.kt
@@ -1,13 +1,15 @@
 package com.raghav.mynotes.models
 
+import android.os.Parcelable
 import androidx.annotation.Keep
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import java.io.Serializable
+import kotlinx.parcelize.Parcelize
 
 @Entity(tableName = "tasks")
 @Keep
+@Parcelize
 data class TaskEntity(
     @PrimaryKey(autoGenerate = true)
     val id: Int? = 0,
@@ -17,4 +19,4 @@ data class TaskEntity(
     // a default value has to be provided for all the records who didn't had this field in version 1
     @ColumnInfo(defaultValue = "0")
     val deadLine: String
-) : Serializable
+) : Parcelable
